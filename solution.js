4d2b4913ff1051a68a44fccc5d8c18726708ac6b
MenaMema <amenabarrera@gmail.com>
2022-07-05 18:41:54 +0200

    finished

---
+++ solution.js
@@ -0,0 +1,22 @@
+console.log(Math.min(-1, 0, 1, 2, 3, 4));
+console.log(Math.max(-1, 0, 1, 2, 3, 4));
+
+//Q2
+
+console.log(Math.ceil(3321.32321));
+console.log(Math.ceil(326.76));
+console.log(Math.ceil(76788.7));
+console.log(Math.ceil(-9.78));
+console.log(Math.ceil(43.342));
+
+console.log("----------");
+
+console.log(Math.floor(3321.32321));
+console.log(Math.floor(326.76));
+console.log(Math.floor(76788.7));
+console.log(Math.floor(-9.78));
+console.log(Math.floor(43.342));
+
+//Q3
+const random = Math.random();
+console.log(Math.ceil(random * 6));
